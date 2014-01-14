478cc59577b17664410bf86b311dc5cbdaca8858
Ben Leichter <benleichter@gmail.com>
2014-01-14 11:57:48 -0500

    Fixed Help Box functionality

--- media/js/libs/helpbox.js
+++ media/js/libs/helpbox.js
@@ -1,3 +1,19 @@
-$(documnet).ready{
-  
-}
+$(document).ready(function(){
+  var p_holder = "Welcome to the Atlas of Economic Complexity. Hover over an element to see it's function";
+  var h3_holder ="Help Box";
+
+  function exit(){
+    $("#helpBoxInner h3").html(h3_holder);
+    $("#helpBoxInner p").html(p_holder);
+  }
+
+
+  $("#viz").mouseenter(function(){
+    $("#helpBoxInner h3").html("Visualizer");
+    $("#helpBoxInner p").html("");
+  });
+
+  $("#viz").mouseleave(function(e){
+    exit();
+  });
+});
