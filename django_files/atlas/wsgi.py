317d3dc463685deb072eb1dbe0f50809e118d5b7
romsson <romain.vuillemot@gmail.com>
2014-04-04 13:47:17 -0400

    Fixed issue with wsgi

--- django_files/atlas/wsgi.py
+++ django_files/atlas/wsgi.py
@@ -6,7 +6,7 @@ workspace = os.path.dirname(project)
 sys.path.append(workspace)
  
 sys.path.append('/usr/local/lib/python2.6/dist-packages/django/')
-sys.path.append('/home/macro/public_html/atlas_economic_complexity/django_files')
+sys.path.append('/srv/www/rc1/observatory_economic_complexity/django_files')
  
 os.environ['DJANGO_SETTINGS_MODULE'] = 'atlas.settings'
 import django.core.handlers.wsgi
