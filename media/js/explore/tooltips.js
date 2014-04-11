14701500f477f85c1441a98b9b523525748dfbef
Ben Leichter <benleichter@gmail.com>
2014-04-11 11:13:47 -0400

    Changed Trade flow options in both queries

--- media/js/explore/tooltips.js
+++ media/js/explore/tooltips.js
@@ -5,7 +5,7 @@ $.ajax({
   cache: false
 })
 
-.done(function( html ) {
+.done(function( html ) { // creates array containing first sentences 
 
 	var text = $("dl dd");
 
@@ -22,15 +22,38 @@ $.ajax({
 
 });
 
-// var newstr = str.replace("what", "<span title=' ' toolTip='2'>what</span>", "gi");
-// $('body').html(newstr);
+var replace = {
+	"Capability Distance": "<span title='0 void' toolTip='2'>Capability Distance</span>",
+	"Diversity": "<span title='0 void' toolTip='2'>Diversity</span>",
+	"Economic Complexity": "<span title='0 void' toolTip='2'>Economic Complexity</span>",
+	"Economic Complexity Indicator": "<span title='0 void' toolTip='2'>Economic Complexity Indicator</span>",	
+	"Expected Growth": "<span title='0 void' toolTip='2'>Expected Growth</span>",	
+	"Nearby (adjacent possible)": "<span title='0 void' toolTip='2'>Nearby (adjacent possible)</span>",
+	"Productive Knowlege": "<span title='0 void' toolTip='2'>Productive Knowlege</span>",
+	"Product Complexity": "<span title='0 void' toolTip='2'>Product Complexity</span>",
+	"Product Complexity Index": "<span title='0 void' toolTip='2'>Product Complexity Index</span>",
+	"The Product Space": "<span title='0 void' toolTip='2'>The Product Space</span>",
+	"Proximity": "<span title='0 void' toolTip='2'>Proximity</span>"
+
+}
+
+for(var key in replace){
+
+	var s = $("#main").html();
+
+	var re = new RegExp(key,"gi");
+
+	var result = s.replace(re, replace[key]);
+
+	$("#main").html(result);
+
+}
 
 $("*").mouseover(function(){
 	if($(this).attr("toolTip")){
 		$(this).attr("title", toolTips[$(this).attr("toolTip")]);
 	}
 });
-
 
 $("*").mouseleave(function(){
 	if($(this).attr("toolTip")){
