a24cef3dfec4fa92c0cd58a866da63264fca1572
Mali Akmanalp <mali@akmanalp.com>
2014-06-16 10:49:35 -0400

    Remove old app() function

--- django_files/observatory/views.py
+++ django_files/observatory/views.py
@@ -731,7 +731,7 @@ def filename_to_url(request, fileName,):
      country1 = data[4]
      country2 = data[5]
      product = data[6]
-     year= data[7]  
+     year= data[7]
      # Build the url
      #url = app_name + "/" + trade_flow + "/" + country1 + "/" + country2 + "/" + product + "/" + year
      url = reverse('observatory.views.explore', kwargs={'app_name': app_name,'trade_flow':trade_flow,'country1':country1,'country2':country2,'product':product,'year':year})
@@ -746,7 +746,7 @@ def filename_to_url(request, fileName,):
      country1 = data[4]
      country2 = data[5]
      product = data[6]
-     year= data[7]  
+     year= data[7]
      # Build the url
      #url = app_name + "/" + trade_flow + "/" + country1 + "/" + country2 + "/" + product + "/" + year
      url = reverse('observatory.views.explore', kwargs={'app_name': app_name,'trade_flow':trade_flow,'country1':country1,'country2':country2,'product':product,'year':year})
@@ -754,14 +754,14 @@ def filename_to_url(request, fileName,):
      response = full_url
      return response
   if app_name1 == "tree" or "pie" or "product":
-     app_name = data[0] + "_" + data[1]  
-     language = data[2] 
+     app_name = data[0] + "_" + data[1]
+     language = data[2]
      prod_class = data[3]
      trade_flow = data[4]
      country1 = data[5]
      country2 = data[6]
      product = data[7]
-     year= data[8]  
+     year= data[8]
      # Build the url
      #url = app_name + "/" + trade_flow + "/" + country1 + "/" + country2 + "/" + product + "/" + year
      url = reverse('observatory.views.explore', kwargs={'app_name': app_name,'trade_flow':trade_flow,'country1':country1,'country2':country2,'product':product,'year':year})
@@ -939,120 +939,6 @@ def download(request):
 
   return response
 
-def app(request, app_name, trade_flow, filter, year):
-  # Get URL query parameters
-  format = request.GET.get("format", False)
-  lang = request.GET.get("lang", False)
-  crawler = request.GET.get("_escaped_fragment_", False)
-
-  country1, country2, product = None, None, None
-  country1_list, country2_list, product_list, year1_list, year2_list, year_interval_list, year_interval = None, None, None, None, None, None, None
-
-  trade_flow_list = ["export", "import", "net_export", "net_import"]
-
-  year1_list = range(1962, 2011, 1)
-  if "." in year:
-    y = [int(x) for x in year.split(".")]
-    year = range(y[0], y[1]+1, y[2])
-    year2_list = year1_list
-    year_interval_list = range(1, 11)
-    year_interval = year[1] - year[0]
-  else:
-    year = int(year)
-
-  json_response = {
-    "year": year,
-    "app": app_name
-  }
-
-  # Bilateral
-  if "." in filter:
-    bilateral_filters = filter.split(".")
-
-    # Country x Product
-    if len(bilateral_filters[1]) > 3:
-      country1 = Country.objects.get(name_3char=bilateral_filters[0])
-      product = Sitc4.objects.get(code=bilateral_filters[1])
-
-      # Lists used for control pane
-      country1_list = Country.objects.get_all(lang)
-      product_list = Sitc4.objects.get_all(lang)
-      trade_flow_list = ["export", "import"]
-
-      article = "to" if trade_flow == "export" else "from"
-      title = "Where did %s %s %s %s?" % (country1.name, trade_flow, product.name_en, article)
-
-      # cspy means country1 / countr2 / show / year
-      if crawler == "" or format == "json":
-        json_response["data"] = Sitc4_ccpy.objects.cspy(country1, product, trade_flow)
-        json_response["attr_data"] = Country.objects.get_all(lang)
-        json_response["title"] = title
-
-    # Country x Country
-    else:
-      country1 = Country.objects.get(name_3char=bilateral_filters[0])
-      country2 = Country.objects.get(name_3char=bilateral_filters[1])
-
-      # Lists used for control pane
-      country1_list = Country.objects.get_all(lang)
-      country2_list = country1_list
-      trade_flow_list = ["export", "import"]
-
-      article = "to" if trade_flow == "export" else "from"
-      title = "What did %s %s %s %s?" % (country1.name, trade_flow, article, country2.name)
-
-      # ccsy means country1 / countr2 / show / year
-      if crawler == "" or format == "json":
-        json_response["data"] = Sitc4_ccpy.objects.ccsy(country1, country2, trade_flow)
-        json_response["attr_data"] = Sitc4.objects.get_all(lang)
-        json_response["title"] = title
-
-  # Product
-  elif len(filter) > 3:
-    product = Sitc4.objects.get(code=filter)
-    product_list = Sitc4.objects.get_all(lang)
-
-    title = "Who %ss %s?" % (trade_flow.replace("_", " "), product.name_en)
-
-    # sapy means show / all / product / year
-    if crawler == "" or format == "json":
-      json_response["data"] = Sitc4_cpy.objects.sapy(product, trade_flow)
-      json_response["attr_data"] = Country.objects.get_all(lang)
-      json_response["title"] = title
-
-  # Country
-  else:
-    country1 = Country.objects.get(name_3char=filter)
-    country1_list = Country.objects.get_all(lang)
-
-    title = "What did %s %s?" % (country1.name, trade_flow.replace("_", " "))
-
-    # casy means country1 / all / show / year
-    if crawler == "" or format == "json":
-      json_response["data"] = Sitc4_cpy.objects.casy(country1, trade_flow)
-      json_response["attr_data"] = Sitc4.objects.get_all(lang)
-      json_response["title"] = title
-
-  # Send data as JSON to browser via AJAX
-  if format == "json":
-    return HttpResponse(json.dumps(json_response))
-
-  # Return page without visualization data
-  return render_to_response("app/index.html", {
-    "title": title,
-    "trade_flow": trade_flow,
-    "country1": country1,
-    "country2": country2,
-    "product": product,
-    "year": year,
-    "trade_flow_list": trade_flow_list,
-    "country1_list": country1_list,
-    "country2_list": country2_list,
-    "product_list": product_list,
-    "year1_list": year1_list,
-    "year2_list": year2_list,
-    "year_interval": year_interval,
-    "year_interval_list": year_interval_list}, context_instance=RequestContext(request))
 
 def app_redirect(request, app_name, trade_flow, filter, year):
   # Corrent for old spelling of tree map as one word
