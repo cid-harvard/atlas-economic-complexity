63fc69eff75783c612ee0e39e0b69705966126ce
Mali Akmanalp <mali@akmanalp.com>
2014-06-16 16:09:02 -0400

    Remove unused blog

--- django_files/atlas/urls.py
+++ django_files/atlas/urls.py
@@ -60,9 +60,6 @@ urlpatterns = patterns('',
   (r'^about/data/$', "observatory.views.data"),
   (r'^about/permissions/$', "observatory.views.permissions"),
   (r'^about/privacy/$', "observatory.views.privacy"),
-  # blog
-  (r'^about/blog/$', "blog.views.blog_index"),
-  url(r'^about/blog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', "blog.views.blog_post_detail", name="blog_post"),
 
   # book ###################################################################
   (r'^book/$', 'observatory.views.book'),
