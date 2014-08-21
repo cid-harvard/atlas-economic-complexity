7809fe1ba5c9199a8fe4d31220553269f77c5878
romsson <romain.vuillemot@gmail.com>
2014-08-21 16:29:37 -0400

    Remove empty query parameters values

--- media/js/explore/viz_general.js
+++ media/js/explore/viz_general.js
@@ -1234,8 +1234,10 @@ var flat_data,
 
     flat_data = flat_data.filter(function(d){ return d.share > 0.00125})
   
-    flat_data.map(function(d){
-      d.world_trade = world_totals[d.year].filter(function(z){ return d.item_id==z.product_id })[0]['world_trade']
+    flat_data.map(function(d) {
+      if(typeof(world_totals[d.year].filter(function(z){ return d.item_id==z.product_id })[0]) != "undefined") {
+        d.world_trade = world_totals[d.year].filter(function(z){ return d.item_id==z.product_id })[0]['world_trade']
+      } 
       d.id = String(d.id)
     })
   
--- media/js/queryparameters.js
+++ media/js/queryparameters.js
@@ -6,20 +6,28 @@ var queryParameters = {}, queryString = location.search.substring(1),
 
 // Creates a map with the query string parameters
 while (m = re.exec(queryString)) {
-    queryParameters[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+  queryParameters[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
 }
 
 
 
 function updateURLQueryParameters() {
+
+  // Remove empty query parameters
+  for(var k in queryParameters){
+    if(queryParameters.hasOwnProperty(k) && !queryParameters[k]){
+      delete queryParameters[k];
+    }
+  }
+
   history.replaceState({}, "Title", window.location.origin+window.location.pathname+"?"+$.param(queryParameters));
 }
 
-  // Add new parameters or update existing ones
-  queryParameters['prod_class'] = prod_class;
-  //queryParameters['details_treemap'] = queryParameters['details_treemap'] || 2;
-  //queryParameters['cat_id'] = queryParameters['cat_id'] || "";
-  //queryParameters['cont_id'] = queryParameters['cont_id'] || "";
-  //queryParameters['show_related'] = Boolean(queryParameters['show_related']) || false;
-  //queryParameters['trade_flow'] = queryParameters['trade_flow'] || "net";
-  //queryParameters['highlight'] = queryParameters['highlight'] || "";
+// Add new parameters or update existing ones
+queryParameters['prod_class'] = prod_class;
+//queryParameters['details_treemap'] = queryParameters['details_treemap'] || 2;
+//queryParameters['cat_id'] = queryParameters['cat_id'] || "";
+//queryParameters['cont_id'] = queryParameters['cont_id'] || "";
+//queryParameters['show_related'] = Boolean(queryParameters['show_related']) || false;
+//queryParameters['trade_flow'] = queryParameters['trade_flow'] || "net";
+//queryParameters['highlight'] = queryParameters['highlight'] || "";
