084ebaa3cf47d959e144d08fb66d844d7c79d596
romsson <romain.vuillemot@gmail.com>
2014-08-26 10:17:55 -0400

    JS utils files for the atlas

---
+++ media/js/utils.js
@@ -0,0 +1,22 @@
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Array.prototype.getUnique = function(){
+   var u = {}, a = [];
+   for(var i = 0, l = this.length; i < l; ++i){
+      if(u.hasOwnProperty(this[i])) {
+         continue;
+      }
+      a.push(this[i]);
+      u[this[i]] = 1;
+   }
+   return a;
+}
