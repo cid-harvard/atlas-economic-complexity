75a5da900e0c12718640fbc9ccb93b228dd7468d
romsson <romain.vuillemot@gmail.com>
2014-09-04 13:44:05 -0400

    Option to visually disable widgets

--- media/js/queryparameters.js
+++ media/js/queryparameters.js
@@ -26,6 +26,7 @@ function updateURLQueryParameters() {
 // Add new parameters or update existing ones
 queryParameters['prod_class'] = prod_class;
 queryParameters['details_treemap'] = queryParameters['details_treemap'] || 2;
+queryParameters['disable_widgets'] = queryParameters['disable_widgets'] || false;
 //queryParameters['cat_id'] = queryParameters['cat_id'] || "";
 //queryParameters['cont_id'] = queryParameters['cont_id'] || "";
 //queryParameters['show_related'] = Boolean(queryParameters['show_related']) || false;
