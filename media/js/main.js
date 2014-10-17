e485dd38a3f6af6fea94f8faeaf96e86a7ac47e1
Gus Wezerek <gwezerek@gmail.com>
2014-10-17 14:12:43 -0400

    Off-screen menu behavior now working.

---
+++ media/js/main.js
@@ -0,0 +1,50 @@
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+(function() {
+
+	var bodyEl = document.body,
+		content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleNav );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleNav );
+		}
+
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleNav();
+			}
+		} );
+	}
+
+	function toggleNav() {
+		if( isOpen ) {
+			$(bodyEl).removeClass( 'show-site-nav' );
+		}
+		else {
+			$(bodyEl).addClass( 'show-site-nav' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
