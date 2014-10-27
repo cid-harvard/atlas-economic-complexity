b890487dc206e51dc06a9ee0a35186e5bc3648c0
gwezerek <gwezerek@gmail.com>
2014-10-27 10:10:33 -0400

    Refactoring and consolidating third-party libs.

--- media/js/views/home.js
+++ media/js/views/home.js
@@ -1,78 +1,109 @@
-
 // SETUP
 
 var selectTemplate = _.template($(".atlas-select-template").html());
+var bodyEl = document.body;
+var openbtn = document.getElementById('open-button');
+var closebtn = document.getElementById('close-button');
+var isOpen = false;
+
+initEvents();
+
+function initEvents() {
+  openbtn.addEventListener('click', toggleNav);
+  if (closebtn) {
+    closebtn.addEventListener('click', toggleNav);
+  }
+
+  // close the menu element if the target is not the menu element or one of its descendants
+  $('.js-content-wrap').on('click', function(e) {
+    var target = e.target;
+    if (isOpen && target !== openbtn) {
+      toggleNav();
+    }
+  });
+}
 
 
 // HELPERS
 
 function populateSelect(options, menu) {
-    // Calculate which dropdowns to populate
-    var toAppendString = "";
+  // Calculate which dropdowns to populate
+  var toAppendString = "";
 
-    // Sort the options alphabetically
-    options.sort(function(a, b) {
-        return a[0] > b[0] ? 1 : -1;
+  // Sort the options alphabetically
+  options.sort(function(a, b) {
+    return a[0] > b[0] ? 1 : -1;
+  });
+
+  _.each(options, function(e, i) {
+    toAppendString += selectTemplate({
+      option: options[i]
     });
+  });
 
-    _.each(options, function(e, i){
-        toAppendString += selectTemplate({ option: options[i] });
-    });
+  menu.html(toAppendString);
+}
 
-    menu.html(toAppendString);
+function toggleNav() {
+  if (isOpen) {
+    $(bodyEl).removeClass('show-site-nav-slidein');
+  } else {
+    $(bodyEl).addClass('show-site-nav-slidein');
+  }
+  isOpen = !isOpen;
 }
 
 
 // HANDLERS
 
 $.ajax({
-    dataType: "json",
-    url: "api/dropdowns/countries/"
+  dataType: "json",
+  url: "api/dropdowns/countries/"
 }).done(function(data) {
-    populateSelect(data, $(".js-select-countries"));
+  populateSelect(data, $(".js-select-countries"));
 });
 
-$('.js-select-countries').on('change', function(){
-    var $this = $(this);
+$('.js-select-countries').on('change', function() {
+  var $this = $(this);
+  ga('send', {
+    'hitType': 'event', // Required.
+    'eventCategory': 'Explore by Country', // Required.
+    'eventAction': 'click', // Required.
+    'eventLabel': $this.find('option:selected').text()
+  });
+  window.location.href = '../explore/tree_map/export/' + $this.val() + '/all/show/2012/';
+});
+
+
+$('.js-select-products').on('change', function() {
+  var $this = $(this);
+  ga('send', {
+    'hitType': 'event', // Required.
+    'eventCategory': 'Explore by Product', // Required.
+    'eventAction': 'click', // Required.
+    'eventLabel': $this.find('option:selected').text()
+  });
+  window.location.href = '../explore/tree_map/export/show/all/' + $this.val() + '/2012/';
+});
+
+sublime.ready(function() {
+  sublime.player('js-hausman-vid').on('start', function(player) {
     ga('send', {
-      'hitType': 'event',          // Required.
-      'eventCategory': 'Explore by Country',   // Required.
-      'eventAction': 'click',      // Required.
-      'eventLabel': $this.find('option:selected').text()
+      'hitType': 'event', // Required.
+      'eventCategory': 'Hausman Video', // Required.
+      'eventAction': 'click', // Required.
+      'eventLabel': 'Play'
     });
-    window.location.href = '../explore/tree_map/export/' + $this.val() + '/all/show/2012/';
+    $('.video-wrap').fadeOut(200);
+  });
 });
 
-
-$('.js-select-products').on('change', function(){
-    var $this = $(this);
-    ga('send', {
-      'hitType': 'event',          // Required.
-      'eventCategory': 'Explore by Product',   // Required.
-      'eventAction': 'click',      // Required.
-      'eventLabel': $this.find('option:selected').text()
-    });
-    window.location.href = '../explore/tree_map/export/show/all/' + $this.val() + '/2012/';
-});
-
-sublime.ready(function(){
-    sublime.player('js-hausman-vid').on('start', function(player) {
-        ga('send', {
-          'hitType': 'event',          // Required.
-          'eventCategory': 'Hausman Video',   // Required.
-          'eventAction': 'click',      // Required.
-          'eventLabel': 'Play'
-        });
-        $('.video-wrap').fadeOut(200);
-    });
-});
-
-$('.track-click').on('click', function(){
-    var $this = $(this);
-    ga('send', {
-      'hitType': 'event',          // Required.
-      'eventCategory': $this.data('ga-category'),   // Required.
-      'eventAction': 'click',      // Required.
-      'eventLabel': $this.data('ga-label')
-    });
+$('.track-click').on('click', function() {
+  var $this = $(this);
+  ga('send', {
+    'hitType': 'event', // Required.
+    'eventCategory': $this.data('ga-category'), // Required.
+    'eventAction': 'click', // Required.
+    'eventLabel': $this.data('ga-label')
+  });
 });
