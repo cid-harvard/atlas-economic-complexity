f4d1d224302695b141c3b17718727ce29846b1aa
romsson <romain.vuillemot@gmail.com>
2015-12-08 16:05:40 -0500

    Changes in JS files

--- media/js/views/home.js
+++ media/js/views/home.js
@@ -87,9 +87,9 @@ $('.js-country-or-product').on('click', function() {
   });
 
   if ( $this.hasClass('js-country') ) {
-    url = '../explore/tree_map/export/' + selected.val() + '/all/show/2013/';
+    url = '../explore/tree_map/export/' + selected.val() + '/all/show/2014/';
   } else if ( $this.hasClass('js-product') ) {
-    url = '../explore/tree_map/export/show/all/' + selected.val() + '/2013/';
+    url = '../explore/tree_map/export/show/all/' + selected.val() + '/2014/';
   }
 
   window.location.href = url;
